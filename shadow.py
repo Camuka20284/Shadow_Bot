fd0b08b3e8ce40d4968cec93876d2c201d31033a
Camuka20284 <149012726+Camuka20284@users.noreply.github.com>
2024-01-05 16:39:45 +0300

    Update shadow.py

--- shadow.py
+++ shadow.py
@@ -1,10 +1,25 @@
 import discord
+import os
+import random
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
 
 @bot.event
 async def on_ready():
@@ -12,10 +27,29 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
+    await ctx.send(f'Merhaba, benim adım {bot.user}! Ben bir botum!')
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    resimler_listesi = os.listdir('images') #['resim1.jpeg', 'resim2.jpeg', 'resim3.jpeg']
+    with open(f'images/{random.choice(resimler_listesi)}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run("TOKEN")
